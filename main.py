6c60859dc42d1ca6af7309d2293892fb7852103a
Rossa.H.Q <40204617@live.napier.ac.uk>
2018-10-08 10:48:31 +0100

    up to individual civs

--- main.py
+++ main.py
@@ -20,30 +20,24 @@ def filter_eras_by_name(era_name, eras = [], *args):
         return era
     abort(404)
 
-#@app.route('/ancient_era/')
-#def ancient_era_page():
-#     civs = read_civs_from_json_file('ancient_era.json')
-#     time_periods = get_unique_time_periods(civs) 
-#     return render_template('ancient_era_page.html', time_periods = time_periods)
-
 @app.route('/<string:era_name>/<string:time_period>')
-def era_region_index_page(time_period):
-    civs = read_civs_from_json_file('ancient_era.json')
+def era_region_index_page(era_name, time_period):
+    civs = read_civs_from_json_file(era_name.lower() + '.json')
     regions = get_unique_regions(civs)
     return render_template('ancient_era_regions_page.html', time_period = time_period,  regions = regions)
 
-@app.route('/ancient_era/<string:time_period>/<string:region>/')
-def ancient_era_civilisations_page(time_period, region):
-    civs = read_civs_from_json_file('ancient_era.json')
+@app.route('/<string:era_name>/<string:time_period>/<string:region>/')
+def era_civilisations_index_page(era_name, time_period, region):
+    civs = read_civs_from_json_file(era_name.lower() + '.json')
     filtered_civs = filter_civs_by_time_and_region(time_period, region , civs)
-    return render_template('ancient_era_civilisations_page.html', time_period = time_period, 
+    return render_template('era_civilisations_page.html', time_period = time_period, 
                            region = region, civs = filtered_civs)
 
-@app.route('/ancient_era/<string:time_period>/<string:region>/<string:civ_name>/')
-def ancient_era_civilisation_page(time_period, region, civ_name):
-    civs = read_civs_from_json_file('ancient_era.json')
+@app.route('/<string:era_name>/<string:time_period>/<string:region>/<string:civ_name>/')
+def civilisation_page(era_name, time_period, region, civ_name):
+    civs = read_civs_from_json_file(era_name.lower() + '.json')
     civ = get_civ_by_name(civ_name, civs)
-    return render_template('ancient_era_civ_page.html', civ = civ)
+    return render_template('civilisation_page.html', civ = civ)
 
 def get_civ_by_name(civ_name, civs = [], *args):
     for civ in civs:
